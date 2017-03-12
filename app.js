062f1ab7fa9ca28136d46c869cf5f2d4f5d0fe66
arthurmyr <arthurmeyer12@gmail.com>
2017-03-12 15:36:24 +0100

    Home page ok

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    app = express(),
+    appPort = 1337;
+
+app.use(express.static(__dirname + '/public'));
+
+app
+.get('/', function(req, res){
+    res.sendFile('public/index.html', { root : __dirname });
+})
+.get('/presentation', function(req, res){
+    res.sendFile('public/presentation.html', { root : __dirname });
+})
+.get('/activites', function(req, res){
+    res.sendFile('public/activites.html', { root : __dirname });
+})
+.get('/contact', function(req, res){
+    res.sendFile('public/contact.html', { root : __dirnamev});
+})
+.use(function(req, res){
+    res.redirect('/');
+})
+
+app.listen(appPort, function(){
+    console.log('listening on *'+appPort);
+});
