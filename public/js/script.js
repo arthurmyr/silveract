7c6775c6ba976054db9b8aec0c07b1d9cb9ca3ab
arthurmyr <arthurmeyer12@gmail.com>
2017-03-13 13:40:35 +0100

    Finish contact page

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,5 @@
-/*$(document).ready(function(){
-            $(window).scroll(function() { 
-                var height = $(window).scrollTop();
-                if(height > 600) {
-                    $("#header").css('background-color', 'transparent');
-                } else {
-                    $("#header").css('background-color', 'white');
-                }
-            });
-});*/
+$(function(){
+    $("section.activite article").click(function(){
+        document.location.href="/activites"; 
+    });
+});
